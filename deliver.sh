8b44ac254c542534e46d5f43f8c09f4e0f73492f
penghe zhang <ly20254@bristol.ac.uk>
2021-08-23 22:44:08 +0800

    modify deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-nohup java -jar eureka/target/eureka-0.0.1-SNAPSHOT.jar &
-nohup java -jar gateway/target/gateway-0.0.1-SNAPSHOT.jar &
-nohup java -jar email/target/email-0.0.1-SNAPSHOT.jar &
-nohup java -jar user/target/user-0.0.1-SNAPSHOT.jar &
-nohup java -jar order/target/order-0.0.1-SNAPSHOT.jar &
-nohup java -jar admin/target/admin-0.0.1-SNAPSHOT.jar &
+nohup java -jar eureka/target/eureka-0.0.1-SNAPSHOT.jar > logs/eureka-log.txt &
+nohup java -jar user/target/user-0.0.1-SNAPSHOT.jar > logs/user-log.txt &
+nohup java -jar order/target/order-0.0.1-SNAPSHOT.jar > logs/order-log.txt &
+nohup java -jar email/target/email-0.0.1-SNAPSHOT.jar > logs/email-log.txt &
+nohup java -jar gateway/target/gateway-0.0.1-SNAPSHOT.jar > logs/gateway-log.txt &
+nohup java -jar admin/target/admin-0.0.1-SNAPSHOT.jar > logs/admin-log.txt &
